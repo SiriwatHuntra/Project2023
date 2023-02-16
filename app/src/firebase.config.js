3c71e7d7ed98b23abafc55874ae1f2f8dbc0c289
Siriwat Huntra <siriwat26room1@gmail.com>
2023-02-16 14:35:40 +0700

    delete trash

---
+++ app/src/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbVbSX-xGBlZl3Bz3MahcY-q5FCbcYizU",
+  authDomain: "charaproject-6ecf4.firebaseapp.com",
+  projectId: "charaproject-6ecf4",
+  storageBucket: "charaproject-6ecf4.appspot.com",
+  messagingSenderId: "631634316122",
+  appId: "1:631634316122:web:a2df28f05b1cd234ca8163"
+};
+
+const app = initializeApp(firebaseConfig);
