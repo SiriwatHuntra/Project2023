3222c24d2bc4229425c71b4238a3b28489fc2aaf
Siriwat Huntra <siriwat26room1@gmail.com>
2023-02-16 14:56:08 +0700

    add crud

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,19 @@
+import { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=> {
+    const getUser = async () => {
+
+    }
+
+    getUser()
+  }, [])
+
   return (
     <div className="App">
 
@@ -9,3 +22,8 @@ function App() {
 }
 
 export default App;
+
+//
+// https://www.youtube.com/watch?v=jCY6DH8F4oc&list=PLpPqplz6dKxUfvM22GXRvYA4s-mvJE0XF&index=1
+//19.43
+//
