a90b35e347e410a81d26bd785e727a839fe2daa9
Siriwat Huntra <siriwat26room1@gmail.com>
2023-02-18 11:18:16 +0700

    try google auth

---
+++ app/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { AuthContextProvider } from "./context/AuthContext";
+
+function App() {
+  return (
+    <div className="App">
+      <AuthContextProvider>
+
+      </AuthContextProvider>
+    </div>
+  );
+}
+
+export default App;
+
+//
+// https://www.youtube.com/watch?v=jCY6DH8F4oc&list=PLpPqplz6dKxUfvM22GXRvYA4s-mvJE0XF&index=1
+//19.43
+//
