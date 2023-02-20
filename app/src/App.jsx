a9336e09668955fe4cda3ca3ee591b3cd1d10765
Siriwat Huntra <siriwat26room1@gmail.com>
2023-02-20 21:04:54 +0700

    Clean

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,37 +1,14 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { AuthContextProvider } from "./context/AuthContext";
-import Home from "./page/Home";
-import Navbar from "./component/NavBar";
-// import Account from "./page/Account";
-import NotFound from "./page/404";
-import Protected from "./component/Protecter"
-import LogIn from "./page/LogIn";
+import { BrowserRouter, Routes, Route }from "react-router-dom";
+
+
 
 function App() {
-
   return (
-    <div className="App">
-      <AuthContextProvider>
-        <Navbar/>
-        <Routes>
 
-          <Route path="/" element={<Home/>}/>
-          <Route path='/Login' element={<LogIn />} />
+        <div>
+          <h1>Hello</h1>
+        </div>
 
-          {/* <Route path="/Account" element={<Account/>}/> */}
-          <Route path="*" element={<NotFound/>}/>
-          <Route
-            path='/Home'
-            element={
-              <Protected>
-                <Home />
-              </Protected>
-            }
-          />
-        </Routes>
-      </AuthContextProvider>
-    </div>
   );
 }
 
