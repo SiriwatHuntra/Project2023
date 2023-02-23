8184e44373246d64ef8b525052df1a2290e11623
Siriwat Huntra <siriwat26room1@gmail.com>
2023-02-23 12:09:51 +0700

    add func

--- app/src/page/Login.jsx
+++ app/src/page/Login.jsx
@@ -1,29 +1,19 @@
 import React from 'react'
-import { auth } from '../firebase';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
-import { useNavigate } from 'react-router-dom';
-import { GoogleButton } from 'react-google-button';
-
 
 function LogIn() {
-
-    const  navigate = useNavigate();
-
-    const signWithGoogle = () => { const provider = new GoogleAuthProvider();
-        signInWithPopup(auth, provider).then(res => {
-        console.log(res)
-        navigate('/')
-
-    }).catch(err => {
-        console.log(err)
-    })}
-
   return (
     <div>
-      <div className='max-w-[240px] m-auto py-4'>
-        <h1>AvatarCreater</h1>
-        <GoogleButton onClick={signWithGoogle} />
-      </div>
+      <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1"/>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
   )
 }
