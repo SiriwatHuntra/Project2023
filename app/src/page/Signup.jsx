e0dfd548aed41e40b766fe66e128992b830b3499
Siriwat Huntra <siriwat26room1@gmail.com>
2023-02-23 12:09:35 +0700

    change LogIn sys

---
+++ app/src/page/Signup.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import { redirect } from 'react-router-dom'
+import { app } from '../firebase'
+
+function Signup() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Signup
