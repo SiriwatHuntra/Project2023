25a5d67bdec8882d44c7bef1ed619824ed94ebc0
Siriwat Huntra <siriwat26room1@gmail.com>
2023-02-23 13:19:51 +0700

    edit context

--- app/src/page/Signup.jsx
+++ app/src/page/Signup.jsx
@@ -1,11 +1,42 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, {useState} from 'react'
-import { redirect } from 'react-router-dom'
-import { app } from '../firebase'
+import { useNavigate } from 'react-router-dom'
+import { app, auth, storage } from '../firebase'
 
-function Signup() {
+const Signup = () => {
+    const [currentUser, setCurrentUser] = useState(Null);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        try{
+            await createUserWithEmailAndPassword(auth, email, password);
+            setCurrentUser(true);
+        }catch(erorr){
+            alert(err)
+        }
+
+        if (currentUser){
+            navigate('Home')
+        }
+    }
+
   return (
     <div>
-
+        <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" name='email' className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        </div>
+        <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" name="password" className="form-control" id="exampleInputPassword1"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
     </div>
   )
 }
