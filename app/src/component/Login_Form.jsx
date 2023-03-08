e2dc8e27f60191d7c1a47474a8d67b5b732e92db
Siriwat Huntra <siriwat26room1@gmail.com>
2023-03-08 09:55:28 +0700

    move Login with form

---
+++ app/src/component/Login_Form.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import UnknowUser from '../source/UnknowUser'
+
+function Login_Form = () => {
+
+    const [err, setErr] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const display
+    }
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Login_Form
