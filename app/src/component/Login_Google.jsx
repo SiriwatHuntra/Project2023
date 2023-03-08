027b44dcdc8c48bb6533e1c961d2c5c5454b267f
Siriwat Huntra <siriwat26room1@gmail.com>
2023-03-08 09:55:33 +0700

    .

---
+++ app/src/component/Login_Google.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { auth } from '../firebase';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom';
+import { GoogleButton } from 'react-google-button';
+
+const LogInWithGoogle = () => {
+
+    const  navigate = useNavigate();
+
+     const signWithGoogle = () => {
+        const provider = new GoogleAuthProvider();
+         signInWithPopup(auth, provider).then((res) => {
+         console.log(res)
+        navigate('/Home')
+
+    }).catch(err => {
+         console.log(err)
+     })}
+
+   return (
+     <div className='container'>
+       <div className='max-w-[240px] m-auto py-4'>
+         <h1>AvatarCreater</h1>
+         <GoogleButton onClick={signWithGoogle} />
+       </div>
+     </div>
+   )
+ }
+
+ export default LogInWithGoogle
