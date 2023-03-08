8ffb207cdf9426aaadea00b2df37aa59efe11aaa
Siriwat Huntra <siriwat26room1@gmail.com>
2023-03-08 09:42:56 +0700

    add store config

--- app/src/firebase.js
+++ app/src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "@firebase/firestore"
 import {getAuth } from "firebase/auth"
+import {getStorage} from "firebase/storage"
 
 const firebaseConfig = {
   apiKey: "AIzaSyAbVbSX-xGBlZl3Bz3MahcY-q5FCbcYizU",
@@ -14,4 +15,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth();
-export const storage  = getFirestore(app);
+export const storage  = getStorage(app);
