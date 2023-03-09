6e44b8828950f81a3974d1a2a5fe720652a3e03f
Siriwat Huntra <siriwat26room1@gmail.com>
2023-03-09 14:11:53 +0700

    added info card

--- app/src/component/Infocard.jsx
+++ app/src/component/Infocard.jsx
@@ -1,25 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { auth, db } from '../firebase';
-import { getDoc, doc, collection, onSnapshot } from "firebase/firestore";
+import { collection, doc, getDoc, onSnapshot } from "firebase/firestore";
 
 function Infocard() {
     const user = auth.currentUser;
-    const docRef = doc(db, 'Users', user.uid)
+    // const docRef = doc(db, 'Users', user.uid)
 
+    //   onSnapshot(docRef, (doc)=> {
+    //     const userName = doc.data().name
+    //     return userName
+    //   });
+    const [userName, setUserName] = useState('');
+    const [userLastName, setUserLastName] = useState('');
+    const [userFaculty, setUserFaculty] = useState('');
+
+    const docRef = doc(db, 'Users', user.uid)
     onSnapshot(docRef, (doc)=> {
-      console.log(doc.data())
-    });
+      const data = doc.data();
+
+      const userName = data.name
+      const userLastName = data.lastName
+      const userFaculty = data.faculty
+
+      setUserName(data.name);
+      setUserLastName(data.lastName);
+      setUserFaculty(data.faculty);
+
+
+    })
+
 
     return (
 <div className="card">
 <h2 classNameName='logo'>User Info</h2>
    <div className="card-body">
-    <span>{user.email}</span>
+    <span>User mail: {user.email}</span>
   </div>
   <div className="card-body">
-    <span>{}</span>
+    <span>Name: {userName}</span>
   </div>
-
+  <div className="card-body">
+    <span>LastName:{userLastName}</span>
+  </div>
+  <div className="card-body">
+    <span>Faculty: {userFaculty}</span>
+  </div>
 </div>
   )
 }
