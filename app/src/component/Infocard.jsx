5cf79f395525a9d1b4999a8cfa2f9db7e14ceba2
Siriwat Huntra <siriwat26room1@gmail.com>
2023-03-10 09:24:41 +0700

    add css to info card and log out button

--- app/src/component/Infocard.jsx
+++ app/src/component/Infocard.jsx
@@ -1,46 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { auth, db } from '../firebase';
 import { doc, onSnapshot } from "firebase/firestore";
+import '../Decorate/infoCard.css';
 
 function Infocard() {
-    const user = auth.currentUser;
-    const [userName, setUserName] = useState('');
-    const [userLastName, setUserLastName] = useState('');
-    const [userFaculty, setUserFaculty] = useState('');
+  const user = auth.currentUser;
+  const [userName, setUserName] = useState('');
+  const [userLastName, setUserLastName] = useState('');
+  const [userFaculty, setUserFaculty] = useState('');
 
-    const docRef = doc(db, 'Users', user.uid)
-    onSnapshot(docRef, (doc)=> {
-      const data = doc.data();
+  const docRef = doc(db, 'Users', user.uid);
+  onSnapshot(docRef, (doc) => {
+    const data = doc.data();
 
-      const userName = data.name
-      const userLastName = data.lastName
-      const userFaculty = data.faculty
+    const userName = data.name;
+    const userLastName = data.lastName;
+    const userFaculty = data.faculty;
 
-      setUserName(data.name);
-      setUserLastName(data.lastName);
-      setUserFaculty(data.faculty);
+    setUserName(data.name);
+    setUserLastName(data.lastName);
+    setUserFaculty(data.faculty);
+  });
 
-
-    })
-
-
-    return (
-<div className="card">
-<h2 classNameName='logo'>User Info</h2>
-   <div className="card-body">
-    <span>User mail: {user.email}</span>
-  </div>
-  <div className="card-body">
-    <span>Name: {userName}</span>
-  </div>
-  <div className="card-body">
-    <span>LastName:{userLastName}</span>
-  </div>
-  <div className="card-body">
-    <span>Faculty: {userFaculty}</span>
-  </div>
-</div>
-  )
+  return (
+    <div className="card">
+      <h2 className="logo">User Info</h2>
+      <div className="card-body">
+        <span>User mail: {user.email}</span>
+      </div>
+      <div className="card-body">
+        <span>Name: {userName}</span>
+      </div>
+      <div className="card-body">
+        <span>Last Name: {userLastName}</span>
+      </div>
+      <div className="card-body">
+        <span>Faculty: {userFaculty}</span>
+      </div>
+    </div>
+  );
 }
 
-export default Infocard
+export default Infocard;
--- app/src/page/Home.jsx
+++ app/src/page/Home.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import Infocard from '../component/Infocard';
 import Navbar from '../component/Navbar';
 import { auth } from '../firebase';
+import '../Decorate/logOutButton.css'
 
 function Home() {
 
@@ -15,9 +16,10 @@ function Home() {
       <h1>Home</h1>
       <Navbar/>
       <Infocard/>
-      <button onClick={()=>signOut(auth).then(navigate("Login"))}>logout</button>
+      <button className="logout-button" onClick={()=>signOut(auth).then(navigate("Login"))}>logout</button>
     </div>
   )
 }
 
 export default Home
+
