46ee566decfc0673effef54906db7ec572447e70
Siriwat Huntra <siriwat26room1@gmail.com>
2023-03-10 09:30:15 +0700

    add form css

--- app/src/component/Form.jsx
+++ app/src/component/Form.jsx
@@ -1,50 +1,49 @@
 import React from 'react'
 import { doc, setDoc } from "firebase/firestore"
-import { db } from '../firebase';
-import {auth} from '../firebase'
+import { db, auth } from '../firebase';
+import "../Decorate/form.css"
+
 function Form() {
-    const user = auth.currentUser;
+  const user = auth.currentUser;
 
-    const handleSubmit = async (e)=> {
-        e.preventDefault();
-        const name = e.target[0].value;
-        const lastName = e.target[1].value;
-        const faculty = e.target[2].value;
-        const uid = user.uid
-        try{
-            await setDoc(doc(db, 'Users', uid),{
-            uid,
-            name,
-            lastName,
-            faculty
-            })
-
-        }catch(err){
-            console.log(err)
-        }
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const lastName = e.target[1].value;
+    const faculty = e.target[2].value;
+    const uid = user.uid
+    try {
+      await setDoc(doc(db, 'Users', uid), {
+        uid,
+        name,
+        lastName,
+        faculty
+      })
+    } catch (err) {
+      console.log(err)
     }
+  }
 
   return (
-    <div>
-    <form onSubmit={handleSubmit}>
+    <div className="container">
+      <form onSubmit={handleSubmit} className="my-5">
         <div className="mb-3">
-            <label for="name " className="form-label">Name </label>
-            <input type="text" className="form-control" id="name"/>
+          <label htmlFor="name" className="form-label">Name</label>
+          <input type="text" className="form-control" id="name" required />
         </div>
 
         <div className="mb-3">
-            <label for="lastName " className="form-label">LastName </label>
-            <input type="text" className="form-control" id="lastName"/>
+          <label htmlFor="lastName" className="form-label">Last Name</label>
+          <input type="text" className="form-control" id="lastName" required />
         </div>
 
         <div className="mb-3">
-            <label for="faculty " className="form-label">Faculty </label>
-            <input type="text" className="form-control" id="faculty"/>
+          <label htmlFor="faculty" className="form-label">Faculty</label>
+          <input type="text" className="form-control" id="faculty" required />
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
+      </form>
     </div>
   )
 }
