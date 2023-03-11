1ad87ebf8b9665b770c047830cbbf07bbbeb7637
Siriwat Huntra <siriwat26room1@gmail.com>
2023-03-11 14:25:02 +0700

    documaentation

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,7 +5,7 @@ import Notfound from "./page/Notfound";
 import Editprofile from "./page/Editprofile";
 import Signup from "./page/Signup";
 import { Routes, Route}from "react-router-dom";
-import ImageSwitcher from "./page/charEditDemo";
+import ImageSwitcher from "./page/ImageSwitcher";
 
 
 function App() {
