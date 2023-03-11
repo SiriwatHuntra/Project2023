a66108f2b3d5d431e156b16bdad07517a255b566
Siriwat Huntra <siriwat26room1@gmail.com>
2023-03-11 13:22:21 +0700

    clean code

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,10 @@
 import Home from "./page/Home";
 import LogIn from "./page/Login";
-import React, { useContext } from "react";
+import React from "react";
 import Notfound from "./page/Notfound";
 import Editprofile from "./page/Editprofile";
 import Signup from "./page/Signup";
-import { Routes, Route, Navigate}from "react-router-dom";
-import { AuthContext } from "./context/AuthContext";
+import { Routes, Route}from "react-router-dom";
 import ImageSwitcher from "./page/charEditDemo";
 
 
